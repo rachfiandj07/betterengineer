22fbc2a4d0a6cae021fc67d766b3afa9a5bb8398
Naufal Djamhur <58916388+rachfiandj07@users.noreply.github.com>
2023-12-12 17:12:20 +0700

    Time: 77 ms (87.91%), Space: 55.8 MB (74.91%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let set = new Set()
+    for ( let i = 0; i<nums.length; i++) {
+        if (set.has(nums[i])) return true;
+        set.add(nums[i])
+        if (set.size > k) set.delete(nums[i - k])
+    }
+    return false
+};
