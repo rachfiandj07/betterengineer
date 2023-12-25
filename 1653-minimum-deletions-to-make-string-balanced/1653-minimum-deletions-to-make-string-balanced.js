e16b9842635fbc6ca096308838108a0938a6b4bd
Naufal Djamhur <58916388+rachfiandj07@users.noreply.github.com>
2023-12-25 18:29:11 +0700

    Time: 91 ms (80.77%), Space: 49.1 MB (50.00%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.js
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.js
@@ -7,7 +7,7 @@ var minimumDeletions = function(s) {
     let count = 0;
     
     for(let i = 0; i < s.length; i++) {
-        if(stack.length && stack[stack.length-1] > s[i]) {
+        if(stack.length && stack[stack.length-1].charCodeAt(0) > s[i].charCodeAt(0)) {
             count += 1;
             stack.pop();
         } else {
