aeba4dbb7f5cb8f6594368f8603396d2feafdd7e
Naufal Djamhur <58916388+rachfiandj07@users.noreply.github.com>
2024-04-08 18:52:12 +0700

    Time: 61 ms (18.73%), Space: 48.6 MB (77.72%) - LeetHub

---
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} students
+ * @param {number[]} sandwiches
+ * @return {number}
+ */
+var countStudents = function(students, sandwiches) {
+    while (students.length > 0 && students.indexOf(sandwiches[0]) !== -1) {
+        if (students[0] === sandwiches[0]) {
+            students.shift()
+            sandwiches.shift()
+        } else {
+            students.push(students.shift())
+        }
+    }
+
+    return students.length
+};
