67d68ff3244c44be89a2d7c11dea4a2da1f81c25
Naufal Djamhur <58916388+rachfiandj07@users.noreply.github.com>
2024-07-25 11:36:57 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} mapping
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortJumbled = function(mapping, nums) {
+    let j = []
+    let m = {}
+    for ( let i = 0; i<nums.length; i++) {
+        m[nums[i]] = i
+    }
+    let t = nums.map(num => {
+
+        return num.toString().split('').map(d => parseInt(d, 10))
+    })
+    for (let i = 0; i<t.length; i++) {
+        let y = ''
+        for ( let x = 0; x<t[i].length; x++) {
+            y += mapping[t[i][x]]
+        }
+        j.push(parseInt(y, 10))
+        y = ''
+    }
+    nums.sort((a, b) => {
+        let freqA = j[m[a]]
+        let freqB = j[m[b]]
+
+        if (freqA !== freqB) {
+            return freqA - freqB
+        } else {
+            return b - a
+        }
+    })
+
+    return nums
+};
